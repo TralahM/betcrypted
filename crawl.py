e9aa18a6e011a149fde7e1ec99e1e69471949245
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-07-17 17:10:25 +0300

    add crawl script template

---
+++ crawl.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from auth import *
+
+browser=webdriver.Firefox('/usr/local/bin/geckodriver')
+
+browser.get(url)
+
+#Find login form
+#send username
+#send password
+#click submit
+
+#go to predictions
+#get data table
+#extract table data
+#save table data csv
+
+#upload table csv to server
+
