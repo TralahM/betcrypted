b586af529b7aa8c0c4dda2b0b36cbc0deb3fa0fa
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-07-17 17:35:58 +0300

    made game migrations

--- games/models.py
+++ games/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Team(models.Model):
-    name=models.CharField(max_length='100')
+    name=models.CharField(max_length=100)
     odds=models.FloatField()
     home=models.BooleanField()
 class Game(models.Model):
-    hometeam=models.ForeignKey(Team,on_delete=models.CASCADE)
-    awayteam=models.ForeignKey(Team,on_delete=models.CASCADE)
+    hometeam=models.ForeignKey(Team,related_name='hometeam',on_delete=models.CASCADE)
+    awayteam=models.ForeignKey(Team,related_name='awayteam',on_delete=models.CASCADE)
     matchdate=models.DateField()
 
