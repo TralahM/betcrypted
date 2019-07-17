28e57f8306c61ccae01f0e63f2734bbe20c650e2
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-07-17 20:50:20 +0300

    update auth

--- crawl.py
+++ crawl.py
@@ -1,19 +1,32 @@
 from selenium import webdriver
-from auth import *
+import auth
 
-browser=webdriver.Firefox('/usr/local/bin/geckodriver')
+browser = webdriver.Firefox('/usr/local/bin/geckodriver')
 
-browser.get(url)
+browser.get(auth.url)
 
-#Find login form
-#send username
-#send password
-#click submit
+# Find login form
+fbutton = browser.find_element_by_xpath('//*[@id="open_log_reg_panel"]')
+fbutton.click()
+lgntab = browser.find_element_by_xpath('//*[@id="login_tab"]')
+lgntab.click()
 
-#go to predictions
-#get data table
-#extract table data
-#save table data csv
+username = browser.find_element_by_xpath(
+    '/html/body/aside[2]/div/div[2]/div[2]/div/form/div[1]/input[1]')
+password = browser.find_element_by_xpath(
+    '/html/body/aside[2]/div/div[2]/div[2]/div/form/div[1]/input[2]')
+loginbtn = browser.find_element_by_xpath(
+    '/html/body/aside[2]/div/div[2]/div[2]/div/form/div[5]/button')
+# send username
+username.send_keys(auth.username)
+# send password
+password.send_keys(auth.password)
+# click submit
+loginbtn.click()
 
-#upload table csv to server
+# go to predictions
+# get data table
+# extract table data
+# save table data csv
 
+# upload table csv to server
